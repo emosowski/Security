1f2dc831e398886ef8beac19d6b3fbc9e5a4ea93
Emily Osowski <emosowski@gmail.com>
2016-11-08 14:33:53 -0500

    Initial Commit

---
+++ Security App/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  Security App
+//
+//  Created by Emily on 11/8/16.
+//  Copyright © 2016 emilyosowski. All rights reserved.
+//
+
+import UIKit
---
+++ Security App/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Security App
+//
+//  Created by Emily on 11/8/16.
+//  Copyright © 2016 emilyosowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet var fiveMustDos: UIButton!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+
+}
+
