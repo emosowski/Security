2f9c0d5b094725a8e33ad863b987273b56563061
Emily Osowski <emosowski@gmail.com>
2016-11-25 09:58:16 -0600

    Add map

---
+++ Security App/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  Security App
+//
+//  Created by Emily on 11/18/16.
+//  Copyright © 2016 emilyosowski. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
+
