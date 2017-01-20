416fc84e853709e9ea064f77be0bccf1a2c14ecd
Osowski <osowskie@E0368095.fios-router.home>
2017-01-20 13:09:26 -0500

    Add push notifications

--- Security App/AppDelegate.swift
+++ Security App/AppDelegate.swift
@@ -7,64 +7,91 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    
+    // BEVideoChat
     let APP_ID = "D55FE643-9856-08BB-FFC4-163728F3F400"
     let SECRET_KEY = "1302ECDA-6678-F016-FF86-D01C9E33EC00"
     let VERSION_NUM = "v1"
     
     var backendless = Backendless.sharedInstance()
-
-    var window: UIWindow?
     
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        backendless?.initApp(APP_ID, secret:SECRET_KEY, version:VERSION_NUM)
-        backendless?.messaging.registerForRemoteNotifications()
-        
-//        
-        UINavigationBar.appearance().barTintColor = UIColor(red: 0.9, green: 0, blue: 0, alpha: 1)
-        UINavigationBar.appearance().tintColor = UIColor.white
-//        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.whiteColor()]
-        
         // Override point for customization after application launch.
+        
+        DebLog.setIsActive(true)
+        
+        backendless!.initApp(APP_ID, secret:SECRET_KEY, version:VERSION_NUM)
+        //backendless!.hostURL = "http://api.backendless.com"
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+        }
+        
         return true
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        backendless?.messaging.registerDeviceToken(deviceToken)
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        // handle error
-    }
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        backendless!.messaging.applicationWillTerminate()
     }
-
-
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        backendless!.messaging.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        backendless!.messaging.didFailToRegisterForRemoteNotificationsWithError(error)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        backendless!.messaging.didReceiveRemoteNotification(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        print("didReceiveRemoteNotification userInfo: \(userInfo)")
+        
+        backendless!.messaging.didReceiveRemoteNotification(userInfo)
+        completionHandler(.newData)
+    }
+    
 }
 
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    // Receive displayed notifications for iOS 10 devices.
+    @objc(userNotificationCenter:willPresentNotification:withCompletionHandler:)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Swift.Void) {
+        
+        let userInfo = notification.request.content.userInfo
+        print(">>>>>>>>>>>>> userNotificationCenter -> willPresent notification: userInfo = \(userInfo)")
+        
+        backendless!.messaging.didReceiveRemoteNotification(userInfo)
+    }
+}
--- Security App/ViewController.swift
+++ Security App/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var cityScroll: UIPickerView!
-    var cities = ["New York, NY", "Redwood City, CA"]
+class ViewController: UIViewController, IBEPushReceiver {
+    
+    var backendless = Backendless.sharedInstance()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if #available(iOS 10.0, *) {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            }
+        }
+        else {
+            let settings = UIUserNotificationSettings(types:[.alert, .sound, .badge], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+        }
+        
+        backendless!.messaging.registerDevice(withChannels: ["testing"])
+        backendless!.messaging.pushReceiver = self
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -22,12 +37,66 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let backItem = UIBarButtonItem()
-        backItem.title = ""
-        navigationItem.backBarButtonItem = backItem // This will show in the next view controller being pushed
+    func publish() {
+        
+        print("TEXT IS PUBLISHED")
+        
+        
+        let publishHeaders = [
+            "publisher_name":"Tester",
+            "ios-badge":"1",
+            "ios-sound":"default",
+            "android-content-text":"Content text",
+            "android-content-title":"Title",
+            "android-ticker-text":"Ticker text"
+            ] as [String : Any]
+        
+        let publishOptions = PublishOptions()
+        publishOptions.assignHeaders(publishHeaders)
+        
+        let deliveryOptions = DeliveryOptions()
+        deliveryOptions.pushPolicy(PUSH_ONLY)
+        deliveryOptions.pushBroadcast(FOR_ANDROID.rawValue|FOR_IOS.rawValue)
+        //deliveryOptions.addSinglecast("AC1B6B88-9061-4BDA-81E9-BBAAA735369C")
+        
+        backendless!.messaging.publish(
+            "testing",
+            message: "Hello, 😄😀😊😈😧", //"ONE",
+            publishOptions: publishOptions,
+            deliveryOptions: deliveryOptions,
+            response: {(status: MessageStatus?) -> () in
+                print("Message has been sent: \(status)")
+        },
+            error: { (fault : Fault?) -> () in
+                print("Server reported an error: \(fault)")
+        })
     }
-
+    
+    
+    // -------------------------------------------------------------------------------------------
+    
+    
+    // IBEPushReceiver Methods
+    
+    public func didReceiveRemoteNotification(_ notification: String!, headers: [AnyHashable : Any]!) {
+        print("RECEIVED MESSAGE: \(headers["publisher_name"]) -> \(notification)")
+    }
+    
+    public func didRegisterForRemoteNotifications(withDeviceId deviceId: String!, fault: Fault!) {
+        
+        if fault == nil {
+            print("didRegisterForRemoteNotificationsWithDeviceId: \(deviceId)")
+            
+            publish()
+        }
+        else {
+            print("didRegisterForRemoteNotificationsWithDeviceId: \(fault)")
+        }
+    }
+    
+    public func didFailToRegisterForRemoteNotificationsWithError(_ err: Error!) {
+        print("didFailToRegisterForRemoteNotificationsWithError: \(err)")
+    }
 
 }
 
